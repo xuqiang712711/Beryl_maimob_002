38b57d9cdc9eafbe90ca1e03f338857187448587
Beryl <beryl.xu@maimob.cn>
2013-11-01 14:08:07 +0800

    edit

--- beryl/src/com/example/beryl/MainActivity.java
+++ beryl/src/com/example/beryl/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.beryl;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,12 +13,21 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final Button btn = (Button) findViewById(R.id.btn);
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			}
+		});
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		
 		return true;
 	}
 
